f4504c2976986c5c8a904564e1d1fd130f87ff44
alkakashyap <Margrita.123>
2020-10-19 13:13:19 +0530

    updated icon and uniqueId changes

--- app/src/main/java/in/onenzeros/shoppinglist/utils/BaseActivity.kt
+++ app/src/main/java/in/onenzeros/shoppinglist/utils/BaseActivity.kt
@@ -1,10 +1,12 @@
 package `in`.onenzeros.shoppinglist.utils
 
 import `in`.onenzeros.shoppinglist.R
+import android.annotation.SuppressLint
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
+import android.provider.Settings.Secure.getString
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import java.util.*
@@ -13,12 +15,17 @@ open class BaseActivity : AppCompatActivity(), ConnectivityReceiver.Connectivity
     private var mSnackBar: Snackbar? = null
     private var clientId: String = ""
     private var mConnectionChangeListener: ConnectionChangeListener? = null
+
+    @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerReceiver(ConnectivityReceiver(),
             IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
         )
         clientId = UUID.randomUUID().toString()
+        clientId = getString(
+            contentResolver,
+            Settings.Secure.ANDROID_ID)
     }
 
     fun setConnectionChangeListener(mConnectionChangeListener: ConnectionChangeListener) {
